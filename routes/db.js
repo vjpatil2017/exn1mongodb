7d8e2d5ca94a528e1a279f0cb31226ab2c90b7f3
vjpatil2017 <vjpatil2017@yahoo.com>
2021-03-10 23:07:37 -0800

    changed1234

--- routes/db.js
+++ routes/db.js
@@ -1,29 +1,24 @@
-
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require("mongodb").MongoClient;
 
-
-
-
-router.get('/', function(req, res, next) {
-  const uri ="mongodb+srv://exn1user:user1@exn1mean.48uvk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"; //process.env.MONGODB_URI;
+router.get("/", function (req, res, next) {
+  const uri = process.env.MONGODB_URI;
 
   MongoClient.connect(uri, function (err, db) {
     if (err) throw err;
-    const database = db.db('sample_stud');
-    const student = database.collection('student');
+    const database = db.db("sample_stud");
+    const student = database.collection("student");
     const query = {};
 
     student.find(query).toArray(function (err, stud) {
       if (err) throw err;
-      res.render('pages/mongodb', {results: stud});
+      res.render("pages/mongodb", { results: stud });
     });
 
     db.close(function (err) {
       if (err) throw err;
     });
-
   });
 });
 
